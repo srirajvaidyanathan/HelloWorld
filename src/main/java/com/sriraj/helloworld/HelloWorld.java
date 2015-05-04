3deca5316ee958f58a3821acd9de22c331ffd81f
Sriraj Vaidyanathan <srivaidyanathan@paypal.com>
2015-05-04 12:13:59 -0700

    First commit

---
+++ src/main/java/com/sriraj/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sriraj.helloworld;
+
+public class HelloWorld {
+
+	public static void main (String args[]) {
+		displayHelloWorld();
+	}
+	
+	public static void displayHelloWorld() {
+		System.out.println("HelloWorld");
+	}
+}
