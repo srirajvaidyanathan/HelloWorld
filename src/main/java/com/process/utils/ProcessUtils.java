6ea3387483f8682fee2e73aa9d4df0f6ae2ef015
Sriraj Vaidyanathan <srivaidyanathan@paypal.com>
2015-06-04 07:57:41 -0700

    Adding ProcessUtils class for util capability

--- src/main/java/com/process/utils/ProcessUtils.java
+++ src/main/java/com/process/utils/ProcessUtils.java
@@ -5,5 +5,9 @@ public class ProcessUtils {
 	public static Integer computeSum(Integer a, Integer b) {
 		return a+b;
 	}
+	
+	public static Integer computeDifference(Integer a, Integer b) {
+		return a-b;
+	}
 
 }
